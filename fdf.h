2695db3b3a823550642a55c05ac37fd11722fb85
Sarah Touitou <stouitou@bess-f4r1s5.clusters.42paris.fr>
2024-03-01 09:38:14 +0100

    oubli 240229

--- fdf.h
+++ fdf.h
@@ -6,12 +6,14 @@
 /*   By: stouitou <stouitou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 11:23:49 by stouitou          #+#    #+#             */
-/*   Updated: 2024/02/21 16:29:52 by stouitou         ###   ########.fr       */
+/*   Updated: 2024/02/29 16:37:30 by stouitou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
+
+# include <stdio.h>
 
 # include "./Libft/libft.h"
 # include "./Minilibx/mlx.h"
@@ -19,16 +21,58 @@
 # include <X11/keysym.h>
 # include <math.h>
 
-typedef struct s_data
+typedef struct s_coord
 {
-	int		prev_x;
-	int		prev_y;
-	int		prev_z_in_x;
-	char	**prev_z_in_y;
-	int		cur_x;
-	int		cur_y;
-	int		cur_z;
-}			t_data;
+	int				x;
+	int				y;
+	int				z;
+	int				abs;
+	int				ord;
+	struct s_coord	*prev_x;
+	struct s_coord	*prev_y;
+	struct s_coord	*next;
+}					t_coord;
+
+typedef struct	s_view
+{
+	int	hor_rotation;
+	int	vert_rotation;
+	int	x_vect_abs;
+	int	x_vect_ord;
+	int	x_vect_angle_hor;
+	int	x_vect_angle_vert;
+	int	y_vect_abs;
+	int	y_vect_ord;
+	int	y_vect_angle_hor;
+	int	y_vect_angle_vert;
+	int	z_vect_abs;
+	int	z_vect_ord;
+	int	z_vect_angle_hor;
+	int	z_vect_angle_vert;
+}	t_view;
+
+typedef struct s_plane
+{
+	int		x_scale;
+	int		y_scale;
+	int		z_scale;
+	t_view	*view;
+	int		width;
+	int		height;
+	int		margin_abs;
+	int		margin_ord;
+	int		xorigin_in_win;
+	int		yorigin_in_win;
+}			t_plane;
+
+typedef struct s_map
+{
+	char	*name;
+	int		grid_length;
+	int		grid_width;
+	int		deepest;
+	int		highest;
+}			t_map;
 
 typedef struct s_file
 {
@@ -44,32 +88,43 @@ typedef struct s_file
 	int		win_margin;
 }			t_file;
 
-typedef struct s_plan
-{
-	int o_abs;
-	int o_ord;
-	int x_abs;
-	int x_ord;
-	int y_abs;
-	int y_ord;
-	int z_abs;
-	int z_ord;
-}		t_plan;
+// typedef struct s_plan
+// {
+// 	int o_abs;
+// 	int o_ord;
+// 	int x_abs;
+// 	int x_ord;
+// 	int y_abs;
+// 	int y_ord;
+// 	int z_abs;
+// 	int z_ord;
+// }		t_plan;
 
 int		main(int argc, char **argv);
+void	check_map(char *file);
 void	check_file_ext(char *str);
-void	parse_file(char *file, t_file *infos);
-void	infos_init(t_file *infos);
-t_img	*init_image(t_xvar **connect, t_file *infos, t_plan *plane);
-int		handle_key_input(int keysym, t_xvar **connect);
-void	create_plane(t_plan *plane, t_file *infos, int height);
-void	create_image(t_xvar **connect, t_img **img, char *file, t_plan *plane);
-void	line_up_up_bresenham(t_img *img, t_plan plane, int abs, int ord);
-void	line_up_left_bresenham(t_img *img, t_plan plane, int abs, int ord);
-void	line_down_down_bresenham(t_img *img, t_plan plane, int abs, int ord);
-void	line_down_left_bresenham(t_img *img, t_plan plane, int abs, int ord);
-void	my_pixel_put(t_img *img, int x, int y, int color);
-void	open_window(t_xvar **connect, t_file *infos, int width, int height);
+void	parse_map(char *file, t_map *map);
+void	init_plane(t_map map, t_plane *plane);
+void	upd_view_vectors(t_plane plane, t_view *view);
+void	put_plane_in_window(t_xvar *connect, t_map map, t_plane *plane);
+void	show_plane(t_xvar *connect, t_plane plane, t_view *view);
+void	draw_vector_pos(t_xvar *connect, t_plane plane, int abs, int ord, int color);
+void	draw_vector_neg(t_xvar *connect, t_plane plane, int abs, int ord, int color);
+// ---------------------------- //
+// t_plane	*init_map_plane();
+// void	infos_init(t_file *infos);
+// t_img	*init_image(t_xvar **connect, t_file *infos, t_plane *plane);
+// int		handle_key_input(int keysym, t_xvar **connect);
+// void	create_plane(t_plane *plane, t_file infos);
+// void	create_image(t_xvar **connect, t_img **img, char *file, t_plane *plane);
+// void	add_coords(t_coord **coord, char **line, int y);
+// void	draw(t_img **img, t_plane plane, t_coord *coord);
+// void	line_up_up_bresenham(t_img *img, t_coord coord, int abs, int ord);
+// void	line_up_left_bresenham(t_img *img, t_coord coord, int abs, int ord);
+// void	line_down_down_bresenham(t_img *img, t_coord coord, int abs, int ord);
+// void	line_down_left_bresenham(t_img *img, t_coord coord, int abs, int ord);
+// void	my_pixel_put(t_img *img, int x, int y, int color);
+void	open_window(t_xvar **connect, t_map map, t_plane plane);
 int		close_window(t_xvar **connect);
 void	clean_and_exit(t_xvar **connect, t_img **img);
 void	free_tab(char **tab);
